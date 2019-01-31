6da510e34c614872c701a99a88fb0168fbc0164e
Tavia Sterling <TaviaSterling@outlook.com>
2019-01-31 11:26:36 -0700

    queen and worker classes

--- BeehiveManagementSystem/Bee.cs
+++ BeehiveManagementSystem/Bee.cs
@@ -8,5 +8,18 @@ namespace BeehiveManagementSystem
 {
     class Bee
     {
+        public const double HoneyUnitsConsumedPerMg = .25;
+
+        public double WeightMg { get; private set; }
+
+        public Bee(double weightMg)
+        {
+            WeightMg = weightMg;
+        }
+
+        virtual public double HoneyConsumptionRate()
+        {
+            return WeightMg * HoneyUnitsConsumedPerMg;
+        }
     }
 }
