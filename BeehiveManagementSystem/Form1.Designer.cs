dd5cf6edf493669910de33262f7d829fe202ce2c
Tavia Sterling <TaviaSterling@outlook.com>
2019-02-01 14:32:40 -0700

    next shift method

--- BeehiveManagementSystem/Form1.Designer.cs
+++ BeehiveManagementSystem/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.report = new System.Windows.Forms.TextBox();
+            this.nextShift = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
             this.SuspendLayout();
             // 
             // workerBeeJob
@@ -40,21 +44,53 @@
             this.workerBeeJob.Size = new System.Drawing.Size(121, 24);
             this.workerBeeJob.TabIndex = 0;
             // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(163, 64);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(52, 22);
+            this.shifts.TabIndex = 1;
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(13, 121);
+            this.report.Multiline = true;
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(158, 101);
+            this.report.TabIndex = 2;
+            // 
+            // nextShift
+            // 
+            this.nextShift.Location = new System.Drawing.Point(186, 155);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(75, 53);
+            this.nextShift.TabIndex = 3;
+            this.nextShift.Text = "Next Shift";
+            this.nextShift.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.report);
+            this.Controls.Add(this.shifts);
             this.Controls.Add(this.workerBeeJob);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.TextBox report;
+        private System.Windows.Forms.Button nextShift;
     }
 }
 
